a957ee21478afee249daeeb0764687316fa56d32
Christian Fochler <fochler@ehealthexperts.de>
2024-05-06 11:05:23 +0200

    Move frontend config to root
    - update webpack config
    - update dependencies

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,6 +1,17 @@
-import { createRoot } from 'react-dom/client';
 import OAuth2ProxyApiTokenComponent from './components/OAuth2ProxyApiTokenComponent';
 
-const container = document.getElementById('app');
-const root = createRoot(container);
-root.render(<OAuth2ProxyApiTokenComponent />);
+window.plugins.push({
+    id: 'nexus-oauth2-proxy-plugin',
+
+    features: [{
+        mode: 'user',
+        path: '/oauth2proxy-apitoken',
+        view: OAuth2ProxyApiTokenComponent,
+        text: 'OAuth2 Proxy API Token',
+        description: 'Access OAuth2 proxy API token',
+        iconCls: 'x-fa fa-key',
+        visibility: {
+            requiresUser: true
+        }
+    }]
+});
