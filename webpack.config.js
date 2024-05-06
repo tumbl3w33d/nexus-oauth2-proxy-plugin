4512a153824b7215546e57622b378ed68312d107
Christian Fochler <fochler@ehealthexperts.de>
2024-05-06 11:05:23 +0200

    Move frontend config to root
    - update webpack config
    - update dependencies

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    devtool: 'source-map',
+    entry: './src/frontend/src',
+    output: {
+        filename: 'nexus-oauth2-proxy-bundle.js',
+        path: path.resolve(__dirname, 'target', 'classes', 'static')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react'] // Ensure React JSX is appropriately transpiled
+                    }
+                }
+            }
+        ]
+    },
+    externals: {
+        react: 'react',
+        axios: 'axios'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    sourceMap: true
+                }
+            })
+        ]
+    }
+};
